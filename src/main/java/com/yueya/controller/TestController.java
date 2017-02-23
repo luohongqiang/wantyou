fbfff5d6574cb99fe73134ec03869e98ac386e0b
luohongqiang <luohongqiang@qq.com>
2017-02-23 16:12:12 +0800

    add

---
+++ src/main/java/com/yueya/controller/TestController.java
@@ -0,0 +1,37 @@
+package com.yueya.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by 02 on 2017/1/10.
+ */
+@Controller
+public class TestController {
+
+
+    @RequestMapping(value = "/testmvc")
+    public String test(){
+        Map map = new HashMap();
+        map.put("name","xiaoluo");
+        map.put("telephone","36889631");
+        System.out.printf("test mvc");
+        System.out.printf("test mvc");
+        System.out.printf("test mvc");
+        System.out.printf("test mvc");
+        return "success";
+    }
+
+    /*@ResponseBody
+    @RequestMapping(value = "/testRequestBody")
+    public Map testRequestBody(){
+        Map map = new HashMap();
+        map.put("name","xiaoluo");
+        map.put("telephone","36889631");
+        return map;
+    }*/
+}
---
+++ src/main/java/com/yueya/util/MD5Util.java
@@ -0,0 +1,37 @@
+package com.yueya.util;
+
+import java.security.MessageDigest;
+
+/**
+ * Created by 02 on 2017/2/9.
+ */
+public class MD5Util {
+    public static String md5(String str) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(str.getBytes());
+            byte b[] = md.digest();
+
+            int i;
+
+            StringBuffer buf = new StringBuffer("");
+            for (int offset = 0; offset < b.length; offset++) {
+                i = b[offset];
+                if (i < 0)
+                    i += 256;
+                if (i < 16)
+                    buf.append("0");
+                buf.append(Integer.toHexString(i));
+            }
+            str = buf.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return str;
+    }
+    public static void main(String[] args) {
+        System.out.println(md5("4407509@qq.com"+"4407509"));
+        System.out.println(md5("mj1"));
+    }
+}
